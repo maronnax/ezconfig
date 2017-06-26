0cda78ce89c3b871f99da629dd9b3c386bb09b50
Nathan Addy <nathan.addy@windfalltechnical.com>
2017-06-26 00:34:31 -0700

    Add a function for selecting from a list of possible files.

--- ezconfig/__init__.py
+++ ezconfig/__init__.py
@@ -6,12 +6,19 @@ def get_userdir_filename(fn_basename):
 
 def get_filename_relative_to_sentinal(path, sentinal, conf_filename):
     while path != "/":
-        print path
         if sentinal in os.listdir(path):
             conf_path = os.path.abspath(os.path.join(path, conf_filename))
             if os.path.isfile(conf_path):
                 return conf_path
             else:
-                return conf_path, False
+                return None
         path = os.path.split(path)[0]
     return None
+
+
+def get_best_filename(*array_of_filenames):
+    for fn in array_of_filenames:
+        if os.path.isfile(fn):
+            return fn
+    else:
+        return None
