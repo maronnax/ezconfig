e9dd83b7a8a5c9b1e20c34394bc64f0abf29a845
Nathan Addy <nathan.addy@windfalltechnical.com>
2017-10-28 14:47:22 -0700

    Add static option for filenames, improved help functionality

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="ezconfig",
-      version="1.2.1",
+      version="1.2.2",
       description="Utility class for reading configuration files.",
       author="Nathan Addy",
       author_email="nathan.addy@windfalltechnical.com",
