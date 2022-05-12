39bd4b47d88f2016f0a99053c54041a60272f5f0
Nathan Addy <nathan.addy@gmail.com>
2022-05-12 13:06:21 -0700

    Bump version to 1.4.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="ezconfig",
-      version="1.4.0",
+      version="1.4.1",
       description="Utility class for reading configuration files.",
       author="Nathan Addy",
       author_email="nathan.addy@windfalltechnical.com",
